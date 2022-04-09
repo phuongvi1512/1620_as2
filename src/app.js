27ba7c113ac945518374f673e15779e83848e97c
Phuong Vi Dang <phuongvi1512@gmail.com>
2022-04-08 20:31:50 -0700

    create save function: add id to textarea, using getelementbyid to get value of textarea, create object newnote, push object to notes

--- src/app.js
+++ src/app.js
@@ -8,28 +8,49 @@ const notes = [
 
 /* create save and delete button */
 const saveButton = document.createElement('button')
-saveButton.innerHTML = 'Save';
+saveButton.innerHTML = 'Save'
+saveButton.id = 'save-button';
 
 
 const deleteButton = document.createElement('button')
-deleteButton.innerHTML = 'Delete';
+deleteButton.innerHTML = 'Delete'
+deleteButton.id = 'delete-button';
+
 
 /* create textarea with save and delete button*/
-const createNoteArea = document.querySelector('.note-container-main .create-note-area')
+
+const createNoteArea = document.querySelector('.icons')
 const writeNoteArea = document.querySelector('.write-note-area')
+const noteArea = `<textarea id="note-text" rows="20" cols="35"></textarea>`
 function addNotearea() {
-  const noteArea = `<textarea rows="20" cols="35"></textarea>`
-  writeNoteArea.insertAdjacentHTML('afterbegin', noteArea)
   writeNoteArea.appendChild(saveButton)
   writeNoteArea.appendChild(deleteButton)
+  writeNoteArea.insertAdjacentHTML('afterbegin', noteArea)
   createNoteArea.remove()
 }
 
 createNoteArea.addEventListener('click', addNotearea)
 
+
+
 /* save function */
 
+const readNoteArea = document.querySelector(".read-note-area")
 
+function save() {
+  const writtenNote = document.getElementById("note-text").value;
+  const line = writtenNote.split('\n');
+  const newNote = {}
+  newNote['title']= line[0]
+  line.shift()
+  let stringLine = line.join('\r\n');
+  newNote['noteBody'] = stringLine
+  console.log(stringLine)
+  notes.push(newNote)
+  console.log(notes)
+}
+
+saveButton.addEventListener('click', save)
 /*
 
 function deleteNote() {
