cbc70a1576796844f4d6ca6c4547891c79d0b25d
Phuong Vi Dang <phuongvi1512@gmail.com>
2022-04-09 20:54:11 -0700

    part 2: modify cancel function to remove textarea and return to original stat, modify save function to add id to newnote object

--- src/app.js
+++ src/app.js
@@ -8,13 +8,13 @@ const notes = [
 
 /* create save and delete button */
 const saveButton = document.createElement('button')
-saveButton.innerHTML = 'Save'
+saveButton.innerHTML = 'save'
 saveButton.id = 'save-button';
 
 
-const deleteButton = document.createElement('button')
-deleteButton.innerHTML = 'Delete'
-deleteButton.id = 'delete-button';
+const cancelButton = document.createElement('button')
+cancelButton.innerHTML = 'cancel'
+cancelButton.id = 'cancel-button';
 
 
 /* create textarea with save and delete button*/
@@ -26,7 +26,7 @@ function addNotearea() {
   const noteArea = `<textarea id="note-text" rows="20" cols="35"></textarea>`
   writeNoteArea.insertAdjacentHTML('beforeend', noteArea)
   writeNoteArea.appendChild(saveButton)
-  writeNoteArea.appendChild(deleteButton)
+  writeNoteArea.appendChild(cancelButton)
   createdIcons.remove()
 }
 
@@ -35,17 +35,23 @@ createdIcons.addEventListener('click', addNotearea)
 
 
 /* delete function */
-function deleteNote() {
-  document.getElementById("note-text").value = '';
+function cancelNote() {
+  saveButton.remove()
+  cancelButton.remove()
+  let node = document.getElementById("note-text");
+  if (node.parentNode) {
+    node.parentNode.removeChild(node)
+  }
+  createNoteArea.appendChild(createdIcons)
 }
 
-deleteButton.addEventListener('click', deleteNote)
+cancelButton.addEventListener('click', cancelNote)
 
 /* save function */
 
 const readNoteArea = document.querySelector(".read-note-area")
 
-function save() {
+function saveNote() {
   const writtenNote = document.getElementById("note-text").value;
   const line = writtenNote.split('\n');
   const newNote = {}
@@ -53,11 +59,12 @@ function save() {
   line.shift()
   let stringLine = line.join('\r\n');
   newNote['noteBody'] = stringLine
+  newNote['id'] = notes.length + 1
   console.log(stringLine)
   notes.push(newNote)
   console.log(notes)
   saveButton.remove()
-  deleteButton.remove()
+  cancelButton.remove()
   let node = document.getElementById("note-text");
   if (node.parentNode) {
     node.parentNode.removeChild(node)
@@ -70,29 +77,4 @@ if (node.parentNode) {
   node.parentNode.removeChild(node);
 }
 */
-saveButton.addEventListener('click', save)
-/*
-
-function deleteNote() {
-  const deleteButton = document.querySelector('.delete')
-  const noteArea = createNoteArea()
-  const writtenNote = `<p>${noteArea.innerValues}</p>`
-  deleteButton.addEventListener('click', noteArea.remove(writtenNote))
-}
-
-function saveNote() {
-  const noteArea = createNoteArea()
-  const noteContainer = createObjectforNote(noteArea)
-  const saveButton = document.querySelector('.save')
-  saveButton.addEventListener('click',noteContainer)
-}
-
-function createObjectforNote(noteArea) {
-  const writtenNote = `
-    <p>${noteArea.innerValues}
-      </p>`;
-  const objectNote = Object.create(writtenNote);
-  return objectNote
-}
-
-*/
+saveButton.addEventListener('click', saveNote)
